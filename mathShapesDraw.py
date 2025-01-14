e7c8e1b66e2f388112c74e12afd97d8977f19e33
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-14 18:09:06 +0300

    new variables added

--- mathShapesDraw.py
+++ mathShapesDraw.py
@@ -1,7 +1,8 @@
 import turtle
 
-kose=0
-koseSay=int(input("Köşe Sayısını giriniz: "))
+kose = 0
+koseSay = int(input("Köşe Sayısını giriniz: "))
+kenarUzunluk = int(input("Köşe Sayısını giriniz: "))
 
 yazi_tahtasi = turtle.Screen()
 yazi_tahtasi.bgcolor("yellow") #Sayfa arkaplan rengi
@@ -11,7 +12,7 @@ turtle_ins = turtle.Turtle() #turtle çalıştır
 
 while(True):
     if kose!=koseSay:
-        turtle_ins.forward(100) #100px ileri git
+        turtle_ins.forward(kenarUzunluk) #100px ileri git
         turtle_ins.left(180-(((koseSay-2)*180)/koseSay)) #sola X derece aci yap
         kose+=1
     else:
