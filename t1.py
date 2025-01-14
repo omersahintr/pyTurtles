593c07666b592c63a3ec30636109bd1a75dcacda
Omer Sahin <bilgi@omersahin.com.tr>
2025-01-14 17:58:06 +0300

    Turtle first commit added

---
+++ t1.py
@@ -0,0 +1,20 @@
+import turtle
+
+kose=0
+koseSay=int(input("Köşe Sayısını giriniz: "))
+
+yazi_tahtasi = turtle.Screen()
+yazi_tahtasi.bgcolor("yellow") #Sayfa arkaplan rengi
+yazi_tahtasi.title("Yazı Tahtası") #Sayfa Başlığı
+
+turtle_ins = turtle.Turtle() #turtle çalıştır
+
+while(True):
+    if kose!=koseSay:
+        turtle_ins.forward(100) #100px ileri git
+        turtle_ins.left(180-(((koseSay-2)*180)/koseSay)) #sola X derece aci yap
+        kose+=1
+    else:
+        break
+
+turtle.done() #turtle kapat
